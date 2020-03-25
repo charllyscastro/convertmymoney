a9ccc9a86a791402a4476afb0c547454f445a3c7
charllyscastro <charllyscastro@gmail.com>
2020-03-25 20:02:24 -0300

    new lib

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const path = require('path')
 
 const convert = require('./lib/convert')
 
+const apiBCB = require('./lib/api.bcb')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 const port = process.env.PORT || 3000 
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async(req, res) => {
+  const cotacao = await apiBCB.getCotacao()
+  res.render('home', {
+    cotacao
+  })
 })
 
 app.get('/cotacao', (req, res) => {
